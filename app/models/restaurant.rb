36cd997ab033c16c0f85ddf0b4aeff6c944a5961
gustavo meneses <gdameneses@gmail.com>
2023-01-24 14:29:07 +0000

    added notes to schema, wired relations between notes and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_one :report
   has_many :workers, dependent: :destroy
   has_many :floorplans, dependent: :destroy
   has_many :stations, dependent: :destroy
-  has_one :report
+  has_many :notes, dependent: :destro
   validates :user_id, uniqueness: true
 end
