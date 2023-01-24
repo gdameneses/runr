a5608cc519ce2db9d835d214fbac3a156a943894
gustavo meneses <gdameneses@gmail.com>
2023-01-24 14:33:16 +0000

    fixed typo in restaurant.rp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,6 @@ class Restaurant < ApplicationRecord
   has_many :workers, dependent: :destroy
   has_many :floorplans, dependent: :destroy
   has_many :stations, dependent: :destroy
-  has_many :notes, dependent: :destro
+  has_many :notes, dependent: :destroy
   validates :user_id, uniqueness: true
 end
