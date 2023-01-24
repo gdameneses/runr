6fb2ad56bead3d4511cbdf559b998c600cb408dd
gustavo meneses <gdameneses@gmail.com>
2023-01-24 17:12:21 +0000

    note create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,15 @@
 class NotesController < ApplicationController
-  def new
+  def create
+    @note = Note.new(note_params)
+    @note.restaurant = current_user.restaurant
+    @note.save
+    authorize @note
+    redirect_to '/dashboard'
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content)
   end
 end
